c2318c122d38d4fdfe666964b38b323b8d76fb7c
AconCavy <aconcavy@gmail.com>
2021-01-23 01:49:21 +0900

    Add All

---
+++ src/Mulinq/Multidimensional/All.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace Mulinq.Multidimensional
+{
+    public static partial class ArrayExtension
+    {
+        /// <summary>
+        ///     Determines whether all elements of a 2d-array satisfy a condition.
+        /// </summary>
+        /// <param name="source">A 2d-array that contains the elements to apply the predicate to.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <returns>
+        ///     true if every element of the source array passes the test in the specified predicate, or if the sequence is
+        ///     empty; otherwise, false.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">source or predicate is null.</exception>
+        public static bool All<TSource>(this TSource[,]? source, Func<TSource, bool>? predicate)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+            foreach (var value in source)
+                if (!predicate(value))
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Determines whether all elements of a 3d-array satisfy a condition.
+        /// </summary>
+        /// <param name="source">A 3d-array that contains the elements to apply the predicate to.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <returns>
+        ///     true if every element of the source array passes the test in the specified predicate, or if the sequence is
+        ///     empty; otherwise, false.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">source or predicate is null.</exception>
+        public static bool All<TSource>(this TSource[,,]? source, Func<TSource, bool>? predicate)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+            foreach (var value in source)
+                if (!predicate(value))
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ test/Mulinq.Test/Multidimensional/AllTests.cs
@@ -0,0 +1,79 @@
+using System;
+using Mulinq.Multidimensional;
+using NUnit.Framework;
+
+namespace Mulinq.Test.Multidimensional
+{
+    public class AllTests
+    {
+        [Test]
+        public void AllTrue2DimTest()
+        {
+            const int n = 3;
+            var sut = new int[n, n];
+            var actual = sut.All(x => x == 0);
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void AllFalse2DimTest()
+        {
+            const int n = 3;
+            var sut = new int[n, n];
+            sut[0, 0] = 1;
+            var actual = sut.All(x => x == 0);
+            Assert.That(actual, Is.False);
+        }
+
+        [Test]
+        public void NullSource2DimTest()
+        {
+            int[,]? sut = null;
+            Assert.Throws<ArgumentNullException>(() => _ = sut.All(x => x == 0));
+        }
+
+        [Test]
+        public void NullPredicate2DimTest()
+        {
+            const int n = 3;
+            Func<int, bool>? predicate = null;
+            var sut = new int[n, n];
+            Assert.Throws<ArgumentNullException>(() => _ = sut.All(predicate));
+        }
+
+        [Test]
+        public void AllTrue3DimTest()
+        {
+            const int n = 3;
+            var sut = new int[n, n, n];
+            var actual = sut.All(x => x == 0);
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void AllFalse3DimTest()
+        {
+            const int n = 3;
+            var sut = new int[n, n, n];
+            sut[0, 0, 0] = 1;
+            var actual = sut.All(x => x == 0);
+            Assert.That(actual, Is.False);
+        }
+
+        [Test]
+        public void NullSource3DimTest()
+        {
+            int[,,]? sut = null;
+            Assert.Throws<ArgumentNullException>(() => _ = sut.All(x => x == 0));
+        }
+
+        [Test]
+        public void NullPredicate3DimTest()
+        {
+            const int n = 3;
+            Func<int, bool>? predicate = null;
+            var sut = new int[n, n, n];
+            Assert.Throws<ArgumentNullException>(() => _ = sut.All(predicate));
+        }
+    }
+}
