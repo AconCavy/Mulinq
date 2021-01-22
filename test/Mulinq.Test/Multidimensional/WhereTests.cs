f9c9ac49a07d6ba442be8f6f2a593980d53620bc
AconCavy <aconcavy@gmail.com>
2021-01-22 23:48:37 +0900

    Add Where 3-Dim

--- test/Mulinq.Test/Multidimensional/WhereTests.cs
+++ test/Mulinq.Test/Multidimensional/WhereTests.cs
@@ -11,7 +11,7 @@ namespace Mulinq.Test.Multidimensional
         [TestCase(2, 2)]
         [TestCase(3, 4)]
         [TestCase(4, 3)]
-        public void WhereTest(int r, int c)
+        public void Where2DimTest(int r, int c)
         {
             static bool Predicate(int x) => x % 2 == 0;
             const int multiplier = 3;
@@ -27,8 +27,25 @@ namespace Mulinq.Test.Multidimensional
             Assert.That(actual, Is.EqualTo(expected));
         }
 
+        [TestCase(1)]
+        [TestCase(3)]
+        public void Where3DimTest(int n)
+        {
+            static bool Predicate(int x) => x % 2 == 0;
+            var sut = new int[n, n, n];
+            for (var i = 0; i < n; i++)
+                for (var j = 0; j < n; j++)
+                    for (var k = 0; k < n; k++)
+                        sut[i, j, k] = i * n * n + j * n + k;
+
+            var expected = Enumerable.Range(0, n * n * n).Where(Predicate);
+            var actual = sut.Where(Predicate);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
         [Test]
-        public void ArgumentNullTest()
+        public void ArgumentNull2DimTest()
         {
             int[,]? sut = null;
             Func<int, bool>? predicate = null;
@@ -36,6 +53,18 @@ namespace Mulinq.Test.Multidimensional
 
             const int n = 3;
             sut = new int[n, n];
+            Assert.Throws<ArgumentNullException>(() => _ = sut.Where(predicate));
+        }
+
+        [Test]
+        public void ArgumentNull3DimTest()
+        {
+            int[,,]? sut = null;
+            Func<int, bool>? predicate = null;
+            Assert.Throws<ArgumentNullException>(() => _ = sut.Where(x => x % 2 == 0));
+
+            const int n = 3;
+            sut = new int[n, n, n];
             Assert.Throws<ArgumentNullException>(() => _ = sut.Where(predicate));
         }
     }
